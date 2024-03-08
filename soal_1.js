2c5e4cdbd484e05edc09aed988d88285df046c78
AgusSuhertian <kingkazel1211@gmail.com>
2024-03-08 20:08:45 +0700

    535230093 Agus Suhertian

--- soal_1.js
+++ soal_1.js
@@ -1,1 +1,25 @@
 console.log("NodeJs")
+
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function displayPrimes() {
+    const primes = [];
+    for (let i = 2; i <= 1000; i++) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+    return primes;
+}
+
+console.log(displayPrimes());
